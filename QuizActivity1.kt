b60bba326e5af7d6809df6bd8d0544977dc0f5da
mvaculik <73494801+mvaculik@users.noreply.github.com>
2023-11-30 22:40:14 +0100

    add file

--- QuizActivity1.kt
+++ QuizActivity1.kt
@@ -8,18 +8,18 @@ import androidx.appcompat.app.AppCompatActivity
 class QuizActivity1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz1)  // Make sure this line matches the layout file name
+        setContentView(R.layout.activity_quiz1) 
 
         val answerButton1: Button = findViewById(R.id.answerButton1)
         answerButton1.setOnClickListener {
-            // Assuming the button answerButton1 has the correct answer "2"
+           
             val userAnswer = answerButton1.text.toString()
             val correctAnswer = "2"
             val score = if (userAnswer == correctAnswer) {
-                // The user selected the correct answer
+              
                 1
             } else {
-                // The user selected the wrong answer
+             
                 0
             }
             val intent = Intent(this, QuizActivity2::class.java)
